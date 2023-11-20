9b0e80a8466b9fdee066ff42c58d91ae2687fc75
surya20000 <141112445+surya20000@users.noreply.github.com>
2023-11-20 22:12:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    {
-  "scripts": {
-    "build": "[my-framework] build --output public"
-  }
-}
     build: {
       outDir: 'build',
     },
     plugins: [react()],
     server: {
-      port:200,
-      host:true
-    }
+      port: 200,
+      host: true,
+    },
   };
 });
